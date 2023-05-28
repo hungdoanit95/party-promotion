426df1a02970ffa9ee54d4cd8008979c6c93169f
Hung Doan <34135664+hungdoanit95@users.noreply.github.com>
2023-05-28 18:20:17 +0700

    update http to https # localhost

--- app/Http/Middleware/VerifyApiCustom.php
+++ app/Http/Middleware/VerifyApiCustom.php
@@ -38,6 +38,16 @@ class VerifyApiCustom
                 ],500);
             }
         }
+        $whitelist = array(
+            '127.0.0.1',
+            '::1'
+        );
+        
+        if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+            if (!$request->secure()) {
+                return redirect()->secure($request->path());
+            }
+        }
         return $next($request);
     }
 }
