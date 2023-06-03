b17ea4195a3652449fa0bd2779584bf5acf060d4
Hung Doan <34135664+hungdoanit95@users.noreply.github.com>
2023-06-03 21:24:04 +0700

    update

--- app/Http/Controllers/PartyController.php
+++ app/Http/Controllers/PartyController.php
@@ -130,17 +130,15 @@ class PartyController extends Controller
         $plan_parties = PlanParty::leftjoin('parties','parties.id','plan_party.party_id')
         ->leftjoin('users','users.id','plan_party.user_id')
         ->where('plan_party.user_id',$request->user_id);
-        if(isset($request)){
-            if(isset($request->type_view)&&$request->type_view == 'view-day'){
-                $plan_parties = $plan_parties->where('parties.organization_date',date('Y-m-d'));
-            }else if(isset($request->type_view)&&$request->type_view == 'view-month'){
-                $plan_parties = $plan_parties->where('parties.organization_date','LIKE',date('Y-m').'%');
-            }
-            if(isset($request->status)){
-                $plan_parties = $plan_parties->where('plan_party.status',$request->status);
-            }
+        if(isset($request->type_view)&&$request->type_view == 'view-day'){
+            $plan_parties = $plan_parties->where('parties.organization_date',date('Y-m-d'));
+        }else if(isset($request->type_view)&&$request->type_view == 'view-month'){
+            $plan_parties = $plan_parties->where('parties.organization_date','LIKE',date('Y-m').'%');
         }else{
             $plan_parties = $plan_parties->where('parties.organization_date','LIKE',date('Y-m').'%');
+        }
+        if(isset($request->status)){
+            $plan_parties = $plan_parties->where('plan_party.status',$request->status);
         }
         $plan_parties = $plan_parties->select(
             'plan_party.*',
